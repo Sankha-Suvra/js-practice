91b4347090d2cfe774a9994c998a5ed8f1c762bb
Sankha-Suvra <113262996+Sankha-Suvra@users.noreply.github.com>
2024-01-06 14:47:12 +0000

    1st commit

---
+++ projects/color_changer_onclick/script.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click', function(e){
+        console.log(e);
+        console.log(e.target);
+        // if (e.target.id === 'red'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+        // if (e.target.id === 'green'){
+        //     body.style.backgroundColor = e.target.id;
+        // }
+        switch (e.target.id) {
+            case 'red':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'green':
+                body.style.backgroundColor = e.target.id;
+                break;
+            
+            default:
+                
+                break;
+        }
+    })
+})
+
